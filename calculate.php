1073c5104a5ba747351cae82a2cc6d4726c94131
Muhammed Salama(GeeKS) <32281997+MohammedSalama@users.noreply.github.com>
2020-11-12 19:43:02 +0200

    Add files via upload

---
+++ calculate.php
@@ -0,0 +1,17 @@
+<form method="GET" action="handle-calculate.php">
+    <input type="number" name="x" placeholder="x">
+    <br>
+
+    <input type="number" name="y" placeholder="y">
+    <br>
+
+    <select name="op">
+        <option value="add">+</option>
+        <option value="sub">-</option>
+        <option value="mul">*</option>
+        <option value="div">/</option>
+    </select>
+    <br>
+
+    <input type="submit" name="zorar" value="calculate">
+</form>
